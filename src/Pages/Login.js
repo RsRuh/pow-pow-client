af288ad94a464b0946edc4c3a4fcddf48ca83fa7
Ruhul Amin <108362143+RsRuh@users.noreply.github.com>
2022-12-20 18:32:57 -0800

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
+        
 
 
         signin(email, password)
